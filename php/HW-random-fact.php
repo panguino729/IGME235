55879f6eece2887421f3e23d18e7c6338ae72c27
Kat <panguino729@gmail.com>
2019-10-15 19:13:50 -0400

    added php ice

---
+++ php/HW-random-fact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Random Fact</title>
+
+    <link rel="stylesheet" href="random-fact-style.css">
+</head>
+
+<body>
+    <main>
+        <h1>Random Quote of The Day</h1>
+        <div class="info" id="about">
+            <p>Reload this page to recieve a new random quote</p>
+        </div>
+        <div class="info" id="quote">
+            
+        </div>
+    </main>
+
+</body>
+
+</html>
