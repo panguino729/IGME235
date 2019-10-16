97d4fe5a236c51216158b85bd10cd31aaf2b5285
Kat <panguino729@gmail.com>
2019-10-15 20:25:09 -0400

    created fact of the day

--- php/HW-fact-of-the-day.php
+++ php/HW-fact-of-the-day.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Random Fact</title>
 
-    <link rel="stylesheet" href="css/random-fact-style.css">
+    <link rel="stylesheet" href="css/fact-of-day-style.css">
 </head>
 
 <body>
@@ -17,14 +17,18 @@
 
         <div class="info" id="quote">
             <?php
+            // -----DATE-----
+            $sdate = date('l', $timestamp = $_SERVER["REQUEST_TIME"]);
+            $days_week = [0 => "Sunday", 1 => "Monday", 2 => "Tuesday", 3 => "Wednesday", 4 => "Thursday", 5 => "Friday", 6 => "Saturday"];
+            $quote_index = array_search($sdate, $days_week);
+
+            echo ("<h2>Quote for $sdate</h2>");
+
+            // -----QUOTES-----
             $quotes = ["Together we can change the world, just one random act of kindness at a time.  - Ron Hall", "It does not matter how slowly you go as long as you do not stop.  - Confucius", "Only I can change my life. No one can do it for me.  - Carol Burnett", "Failure will never overtake me if my determination to succeed is strong enough.  - Og Mandino", "Life is 10% what happens to you and 90% how you react to it.  - Charles R. Swindoll", "Good, better, best. Never let it rest. 'Til your good is better and your better is best.  - St. Jerome", "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.  - Helen Keller"];
-            echo $quotes[rand(0, count($quotes) - 1)];
+            echo $quotes[$quote_index];
             ?>
         </div>
-
-        <form action="HW-random-fact.php" method="post">
-            <input type="submit" value="Get another Quote!">
-        </form>
     </main>
 </body>
 
