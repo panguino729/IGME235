bfca6e14e88f5b5115ad6aaf81eba0a6960d185a
Kat <panguino729@gmail.com>
2019-11-26 20:18:55 -0500

    Project2

--- project2/scripts/dogScript.js
+++ project2/scripts/dogScript.js
@@ -2,16 +2,11 @@
 
 let dogBreedList;
 
-const breedField = $("#searchterm");
-const subBreedField = $("#subbreed");
-const limitField = $("#limit");
-
 //storage stuff
-const prefix = "eiz4178-"; // change 'abc1234' to your banjo id
+const prefix = "IGME235Project2-";
 const breedKey = prefix + "breed"
 const subBreedKey = prefix + "subBreed";
 const limitKey = prefix + "limit";
-
 
 window.onload = init;
 
@@ -32,14 +27,14 @@ function init() {
         $("#subbreed").val(storedSubBreed);
     }
     else {
-        $("#subbreed").val("not used");
+        $("#subbreed").val("not used");  // a default value if subBreedField is not found
     }
 
     // if we find a previously set limit value, display it
     if (storedLimit) {
         $("#limit").val(storedLimit);
     } else {
-        $("#limit").val("24"); // a default value if limitField is not found
+        $("#limit").val("25"); // a default value if limitField is not found
     }
 
     //Get entire list of breeds and their subbreeds
@@ -53,9 +48,11 @@ function init() {
         }
     })
 
-    document.querySelector("#search").onclick = getData;
+    //Call getData when the search button is clicked
+    document.querySelector("#searchButton").onclick = getData;
 }
 
+///Get's the json object
 function getData() {
     // 1 - main entry point to web service
     const SERVICE_URL = "https://dog.ceo/api/breed/";
@@ -73,18 +70,22 @@ function getData() {
 
     displayTerm = breed;
 
-    if (subBreed == "not used" || subBreed == "") {
+    //If no breed is entered, remind the user
+    if (breed == "" || breed == "not used") {
+        document.querySelector("#content").innerHTML = "Please enter a Breed name";
+    }
+
+    //If no subBreed is entered, just search Breed
+    else if (subBreed == "not used" || subBreed == "") {
         // 3 - parse the user entered term we wish to search
         // not necessary for this service endpoint
         breed = breed.trim();
+        breed = breed.toLowerCase();
         breed = encodeURIComponent(breed);
         if (breed.length < 1) return;
         url = SERVICE_URL + breed + "/images/random/" + limit;
-
-        localStorage.setItem(breedKey, breed);
-        localStorage.elete
-        localStorage.setItem(limitKey, limit);
     }
+    //Search both Breed and subBreed
     else {
         breed = breed.trim();
         breed = encodeURIComponent(breed);
@@ -94,14 +95,15 @@ function getData() {
         subBreed = encodeURIComponent(subBreed);
         if (subBreed.length < 1) return;
         url = SERVICE_URL + breed + "/" + subBreed + "/images/random/" + limit;
-
-        localStorage.setItem(breedKey, breed);
-        localStorage.setItem(subBreedKey, subBreed);
-        localStorage.setItem(limitKey, limit);
     }
 
+    //Save data from input fields to local storage
+    localStorage.setItem(breedKey, breed);
+    localStorage.setItem(subBreedKey, subBreed);
+    localStorage.setItem(limitKey, limit);
+
     // 4 - update the UI
-    document.querySelector("#debug").innerHTML = `<b>Querying web service with:</b> <a href="${url}" target="_blank">${url}</a>`;
+    // document.querySelector("#debug").innerHTML = `<b>Querying web service with:</b> <a href="${url}" target="_blank">${url}</a>`;
 
     // 5- call the web service, and prepare to download the file
     $.ajax({
@@ -110,13 +112,15 @@ function getData() {
         data: null,
         success: jsonLoaded,
         error: function (err) {
+            document.querySelector("#content").innerHTML = "Please enter a valid Breed and/or Sub Breed";
             console.error(err.responseTest);
         }
     })
 
-    console.log(url);
+    // console.log(url);
 };
 
+///Gets a list of all breeds and their respective subBreeds
 function seperateList(obj) {
     if (obj.status != "success") {
         document.querySelector("#content").innerHTML = "<p><i>There was a problem!</i></p>";
@@ -126,7 +130,7 @@ function seperateList(obj) {
     let breedList = Object.keys(obj.message);
 }
 
-
+//Get the results to display on the screen
 function jsonLoaded(obj) {
     // 6 - if there are no results, print a message and return
     // Here, we don't get an array back, but instead a single object literal with 2 properties
@@ -143,19 +147,13 @@ function jsonLoaded(obj) {
 
     for (let i = 0; i < results.length; i++) {
         let result = results[i];
-        // let smallURL = result.images.fixed_width_small.url;
         let smallURL = result;
-
         let url = result;
-        // let line = `<div class='result'><a target='_blank' href='${url}'><img src='${smallURL}' title='${result.id}' />`;
         let line = `<a target='_blank' href='${url}'><div class='result' style='background-image: url(${smallURL})'>`;
-        // let line = "<div class='result' style='background-image: url(" + smallURL + ")'></div>";
         line += `</div></a>`;
 
         bigString += line;
     }
-
-    // let src = obj.message;
 
     // 8 - display final results to user
     document.querySelector("#content").innerHTML = bigString;
